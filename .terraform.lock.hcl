c4dcaf7454ba16a5369ae7c572e347469185b49e
braheezy <michael.c.braha@gmail.com>
2023-04-16 15:11:23 -0700

    Add DynamoDB, Lambda, and API Gateway support

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -1,6 +1,25 @@
 # This file is maintained automatically by "terraform init".
 # Manual edits may be lost in future updates.
 
+provider "registry.terraform.io/hashicorp/archive" {
+  version = "2.3.0"
+  hashes = [
+    "h1:OmE1tPjiST8iQp6fC0N3Xzur+q2RvgvD7Lz0TpKSRBw=",
+    "zh:0869128d13abe12b297b0cd13b8767f10d6bf047f5afc4215615aabc39c2eb4f",
+    "zh:481ed837d63ba3aa45dd8736da83e911e3509dee0e7961bf5c00ed2644f807b3",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:9f08fe2977e2166849be24fb9f394e4d2697414d463f7996fd0d7beb4e19a29c",
+    "zh:9fe566deeafd460d27999ca0bbfd85426a5fcfcb40007b23884deb76da127b6f",
+    "zh:a1bd9a60925d9769e0da322e4523330ee86af9dc2e770cba1d0247a999ef29cb",
+    "zh:bb4094c8149f74308b22a87e1ac19bcccca76e8ef021b571074d9bccf1c0c6f0",
+    "zh:c8984c9def239041ce41ec8e19bbd76a49e74ed2024ff736dad60429dee89bcc",
+    "zh:ea4bb5ae73db1de3a586e62f39106f5e56770804a55aa5e6b4f642df973e0e75",
+    "zh:f44a9d596ecc3a8c5653f56ba0cd202ad93b49f76767f4608daf7260b813289e",
+    "zh:f5c5e6cc9f7f070020ab7d95fcc9ed8e20d5cf219978295a71236e22cbb6d508",
+    "zh:fd2273f51dcc8f43403bf1e425ba9db08a57c3ddcba5ad7a51742ccde21ca611",
+  ]
+}
+
 provider "registry.terraform.io/hashicorp/aws" {
   version     = "4.59.0"
   constraints = "4.59.0"
@@ -40,5 +59,43 @@ provider "registry.terraform.io/hashicorp/http" {
     "zh:d0ba69b62b6db788cfe3cf8f7dc6e9a0eabe2927dc119d7fe3fe6573ee559e66",
     "zh:e28d24c1d5ff24b1d1cc6f0074a1f41a6974f473f4ff7a37e55c7b6dca68308a",
     "zh:fde8a50554960e5366fd0e1ca330a7c1d24ae6bbb2888137a5c83d83ce14fd18",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/local" {
+  version = "2.4.0"
+  hashes = [
+    "h1:R97FTYETo88sT2VHfMgkPU3lzCsZLunPftjSI5vfKe8=",
+    "zh:53604cd29cb92538668fe09565c739358dc53ca56f9f11312b9d7de81e48fab9",
+    "zh:66a46e9c508716a1c98efbf793092f03d50049fa4a83cd6b2251e9a06aca2acf",
+    "zh:70a6f6a852dd83768d0778ce9817d81d4b3f073fab8fa570bff92dcb0824f732",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:82a803f2f484c8b766e2e9c32343e9c89b91997b9f8d2697f9f3837f62926b35",
+    "zh:9708a4e40d6cc4b8afd1352e5186e6e1502f6ae599867c120967aebe9d90ed04",
+    "zh:973f65ce0d67c585f4ec250c1e634c9b22d9c4288b484ee2a871d7fa1e317406",
+    "zh:c8fa0f98f9316e4cfef082aa9b785ba16e36ff754d6aba8b456dab9500e671c6",
+    "zh:cfa5342a5f5188b20db246c73ac823918c189468e1382cb3c48a9c0c08fc5bf7",
+    "zh:e0e2b477c7e899c63b06b38cd8684a893d834d6d0b5e9b033cedc06dd7ffe9e2",
+    "zh:f62d7d05ea1ee566f732505200ab38d94315a4add27947a60afa29860822d3fc",
+    "zh:fa7ce69dde358e172bd719014ad637634bbdabc49363104f4fca759b4b73f2ce",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/random" {
+  version = "3.5.1"
+  hashes = [
+    "h1:VSnd9ZIPyfKHOObuQCaKfnjIHRtR7qTw19Rz8tJxm+k=",
+    "zh:04e3fbd610cb52c1017d282531364b9c53ef72b6bc533acb2a90671957324a64",
+    "zh:119197103301ebaf7efb91df8f0b6e0dd31e6ff943d231af35ee1831c599188d",
+    "zh:4d2b219d09abf3b1bb4df93d399ed156cadd61f44ad3baf5cf2954df2fba0831",
+    "zh:6130bdde527587bbe2dcaa7150363e96dbc5250ea20154176d82bc69df5d4ce3",
+    "zh:6cc326cd4000f724d3086ee05587e7710f032f94fc9af35e96a386a1c6f2214f",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:b6d88e1d28cf2dfa24e9fdcc3efc77adcdc1c3c3b5c7ce503a423efbdd6de57b",
+    "zh:ba74c592622ecbcef9dc2a4d81ed321c4e44cddf7da799faa324da9bf52a22b2",
+    "zh:c7c5cde98fe4ef1143bd1b3ec5dc04baf0d4cc3ca2c5c7d40d17c0e9b2076865",
+    "zh:dac4bad52c940cd0dfc27893507c1e92393846b024c5a9db159a93c534a3da03",
+    "zh:de8febe2a2acd9ac454b844a4106ed295ae9520ef54dc8ed2faf29f12716b602",
+    "zh:eab0d0495e7e711cca367f7d4df6e322e6c562fc52151ec931176115b83ed014",
   ]
 }
--- main.tf
+++ main.tf
@@ -38,6 +38,10 @@ provider "aws" {
     }
   }
 }
+resource "random_string" "random" {
+  length  = 4
+  special = false
+}
 /*
 *
 * S3
@@ -45,7 +49,8 @@ provider "aws" {
 */
 # Create the bucket
 resource "aws_s3_bucket" "main" {
-  bucket = "resume.${var.domain}"
+  bucket        = "resume.${var.domain}"
+  force_destroy = true
 }
 # Attach proper permissions for static websites
 resource "aws_s3_bucket_policy" "main" {
@@ -80,7 +85,7 @@ resource "aws_s3_bucket_website_configuration" "main" {
 # Download the latest resume, for upload
 # This is only for the first deployment, not how every resume update is deployed
 data "http" "resume" {
-  url = "https://raw.githubusercontent.com/braheezy-resume/resume/main/resume.html"
+  url = "https://github.com/braheezy-resume/resume/releases/latest/download/resume.html"
 }
 # Upload resume to bucket
 resource "aws_s3_object" "object" {
@@ -200,4 +205,190 @@ resource "aws_route53_record" "a" {
     zone_id                = aws_cloudfront_distribution.main.hosted_zone_id
     evaluate_target_health = false
   }
+}
+/*
+*
+* DynamoDB
+*
+*/
+resource "aws_dynamodb_table" "main" {
+  name           = "site-analytics"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "metrics"
+
+  attribute {
+    name = "metrics"
+    type = "S"
+  }
+}
+resource "aws_dynamodb_table_item" "init" {
+  table_name = aws_dynamodb_table.main.name
+  hash_key   = aws_dynamodb_table.main.hash_key
+
+  item = <<ITEM
+{
+  "metrics": {"S": "resume.${var.domain}"},
+  "visitorCount": {"N": "0"}
+}
+ITEM
+}
+/*
+*
+* Lambda
+*
+*/
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket_prefix = "resume-lambda"
+  force_destroy = true
+}
+resource "aws_s3_bucket_acl" "private_bucket" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+  acl    = "private"
+}
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambdaDBAccess"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+resource "aws_iam_policy" "lambda_exec" {
+  name = "lambda-db-access-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "dynamodb:GetItem",
+        "dynamodb:PutItem",
+        "dynamodb:UpdateItem",
+        "dynamodb:Scan",
+        "dynamodb:DescribeTable"
+      ]
+      Resource = "arn:aws:dynamodb:*:*:table/${aws_dynamodb_table.main.name}"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.lambda_exec.arn
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  output_path = "${path.module}/src.zip"
+
+  source {
+    content  = "hello"
+    filename = "dummy"
+  }
+}
+resource "aws_lambda_function" "count" {
+  function_name = "count"
+  description   = "Handle logic to get/update visitor count to resume.${var.domain}"
+
+  filename = data.archive_file.lambda_zip.output_path
+
+  runtime = "go1.x"
+  handler = "count"
+
+  role = aws_iam_role.lambda_exec_role.arn
+
+  environment {
+    variables = {
+      DDB_TABLE = aws_dynamodb_table.main.name
+    }
+  }
+  depends_on = [aws_cloudwatch_log_group.lambda_logs]
+
+}
+resource "aws_lambda_function_url" "count" {
+  function_name      = aws_lambda_function.count.function_name
+  authorization_type = "AWS_IAM"
+}
+resource "aws_cloudwatch_log_group" "lambda_logs" {
+  name = "/aws/lambda/resume-analytics"
+
+  retention_in_days = 7
+}
+/*
+*
+* API Gateway
+*
+*/
+resource "aws_apigatewayv2_api" "lambda" {
+  name          = "resume-api"
+  protocol_type = "HTTP"
+}
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.lambda.id
+  name        = "$default"
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gw.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+      }
+    )
+  }
+  depends_on = [aws_cloudwatch_log_group.api_gw]
+}
+resource "aws_apigatewayv2_integration" "apigw_lambda" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  integration_uri    = aws_lambda_function.count.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+resource "aws_apigatewayv2_route" "post" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  route_key = "POST /count"
+  target    = "integrations/${aws_apigatewayv2_integration.apigw_lambda.id}"
+}
+resource "aws_cloudwatch_log_group" "api_gw" {
+  name = "/aws/api_gw/resume-api"
+
+  retention_in_days = 7
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.count.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
 }
